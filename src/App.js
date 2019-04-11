edd05bef7d04dda4f1045aa01829ce0a2ea82bc8
Sujan <pokharelsujan134@gmail.com>
2019-04-11 16:08:30 +0300

    to do in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      message:"Hello Samikshya",
+      newTodo:""
+
+    };
+  }
+  newTodoChanged(event){
+    console.log(event.target.value);
+    this.setState({})
+  }
+
+  formSubmitted(event){
+    event.preventDefault();
+    console.log('form Submitted');
+
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h3> {this.state.message}</h3>
+      <form onSubmit= {this.formSubmitted}>
+        <label htmlFor="newTodo">New Todo </label>
+        <input onChange = {this.newTodoChanged}id="newTodo" name = "newTodo"/>   
+        <button type= "submit">Add Todo</button> 
+      </form>
+
+
+       
       </div>
     );
   }
